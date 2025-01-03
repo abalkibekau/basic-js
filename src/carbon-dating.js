08814878da028be60075ef12ee33d60d95dbb754
abalkibekau <abal.rschool@gmail.com>
2025-01-03 20:49:05 +0100

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity != 'string') {
+    return false;
+  }
+  let activity = Number(sampleActivity)
+  if (!Number.isFinite(activity) || activity <= 0 || activity > MODERN_ACTIVITY) {
+    return false;
+  }
+  let k = Math.LN2/HALF_LIFE_PERIOD;
+  return Math.ceil(
+    Math.log(MODERN_ACTIVITY/sampleActivity)/k
+  );
 }
 
 module.exports = {
