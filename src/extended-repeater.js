e7393f202586824508e911eac994169f05b8a760
abalkibekau <abal.rschool@gmail.com>
2025-01-05 22:03:57 +0100

    feat: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let addition = repeat(options.addition, options.additionRepeatTimes, options.additionSeparator != undefined ? options.additionSeparator : '|');
+  return repeat(str + addition, options.repeatTimes, options.separator != undefined ? options.separator : '+');
+}
+
+function repeat(str, times, separator) {
+  if (str === undefined) {
+    return '';
+  }
+  let value = String(str);
+  if (value == '') {
+    return '';
+  }
+  if (!times) {
+    return value;
+  }
+  let arr = Array(times);
+  arr.fill(value);
+  return arr.join(separator);
 }
 
 module.exports = {
