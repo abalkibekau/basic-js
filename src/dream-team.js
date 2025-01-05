f013da2d01a23e0c0fe2b998ffd2a3524d7b551d
abalkibekau <abal.rschool@gmail.com>
2025-01-05 20:57:37 +0100

    feat: create dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members) || members.length == 0) {
+    return false;
+  }
+
+  let firstLetters = [];
+  for (const member of members) {
+    if (typeof member === 'string') {
+      let name = member.trim();
+      if (name !== '') {
+        firstLetters.push(name[0].toUpperCase())
+      }
+    }
+  }
+  return firstLetters.sort().join('');
 }
 
 module.exports = {
